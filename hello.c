e55f9e71ecf3bc94560246398cb30bbc5b1befa8
jhap95631-prog <jhap95631@gmail.com>
2026-07-28 07:27:23 +0000

    my some basics calculation

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 # include<stdio.h>
 
 int main() {
-    printf("hello world");
+    int a, b;
+    printf("enter a");
+    scanf("%d" ,&a);
+
+    printf("enter b");
+    scanf("%d" , &b);
+    
+    int sum =a+b;
+    printf("sum is: %d", sum);
     return 0;
 }
