ec30e9fb7b796541878f01d192a57b5edcdea365
jhap95631-prog <jhap95631@gmail.com>
2026-07-30 17:48:24 +0000

    some operation

--- hello.c
+++ hello.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
-#include<math.h>
+# include <stdio.h>
+# include <math.h>
 
-int main()  {
+int main () {
     int x;
-    printf("enter a number: ");
-    scanf("%d", &x);
-    printf("%d", x % 2 ==0);
+    printf("enter number : ");
+    scanf("%d" ,&x);
+    printf("%d \n", x>9 && x<100);
     return 0;
-}
+}    
+
+
+    
 
    
 
