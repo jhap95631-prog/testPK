9db3d79eb40e03a587d8c4ebd302d9c3cb22a928
jhap95631-prog <jhap95631@gmail.com>
2026-08-05 04:43:45 +0000

    print 1 -100 number by using loop

--- homework.c
+++ homework.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 
 int main () {
-    for (int i=1; i<1000; i=i+1) {
-        printf("hello world \n");
+    for (int i=1; i<100; i=i+1) {
+        printf("%d \n", i);
     }    
     return 0;
 }    
