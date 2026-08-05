b016f0f54f55832b4ea264bbf91c440f27e7b34a
jhap95631-prog <jhap95631@gmail.com>
2026-08-05 04:38:18 +0000

    print hello world for 1000 times by using loops

--- homework.c
+++ homework.c
@@ -1,26 +1,10 @@
 # include <stdio.h>
 
 int main () {
-    int marks;
-    printf("enter number (0-100): ");
-    scanf("%d", &marks);
-
-    if(marks <30) {
-        printf("C \n");
-    }
-    else if(marks >= 30 && marks < 70) {
-        printf("B \n");
-    }   
-    else if(marks >= 70 && marks < 90) {
-          printf("A \n");
-    }
-    else {     
-         printf("A+ \n");
-    }
-    
+    for (int i=1; i<1000; i=i+1) {
+        printf("hello world \n");
+    }    
     return 0;
-
 }    
-
         
     
