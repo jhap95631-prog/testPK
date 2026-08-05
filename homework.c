5dc0ebcf2d60bd78ddfb0b8048c56868cbe47278
jhap95631-prog <jhap95631@gmail.com>
2026-08-05 11:24:16 +0000

    float and character  through loop counter

--- homework.c
+++ homework.c
@@ -1,17 +1,2 @@
-# include <stdio.h>
-
-int main () {
-    //increment operator
-    //++i (pre increment)
-    // i++ (post increment )
     
-    int i=1;
-    // printf(" \n", i++); // use, then increase
-    // printf("%d \n");
-
-    printf("%d \n", ++i); // increase ,then use
-    printf("%d \n", i);
-    return 0;
-}    
-        
     
