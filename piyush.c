75ab06c16b8666f6ce1b729207bf07bfe47bde84
jhap95631-prog <jhap95631@gmail.com>
2026-08-09 07:29:16 +0000

    print the sum of first n natural number n=4

--- piyush.c
+++ piyush.c
@@ -1,20 +1,25 @@
-# include<stdio.h>
-
-int main()  {
+#include<stdio.h>
+int main() {
     int n;
     printf("enter number : ");
-    scanf("%d", &n);
+    scanf("%d",&n);
 
-    int i=0;
-    while(i <= n) {
-        printf("%d\n",i);
-        i++;
-
-    }
-
+    int sum = 0;
     for(int i=1; i<=n; i++) {
-        printf("%d\n",i);
-    } 
+        sum=sum + i;// sum += i
+    }
+        
+    printf("sum is %d \n",sum);
+
     return 0;
 }    
+
+
+
+   
+    
+
+
+
+    
 
