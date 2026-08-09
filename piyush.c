10647d2de3a15d146fe787b519d169e0671de3c5
jhap95631-prog <jhap95631@gmail.com>
2026-08-09 04:09:09 +0000

    print 0 to 6 number by using  while using

--- piyush.c
+++ piyush.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
+# include<stdio.h>
 
-int main() {
-    for(float i=1.0; i<=5.0; i++) {
-        printf("%f \n", i);
-    } 
-    
-    for(char ch='a'; ch<='z'; ch++) {
-        printf("%c \n", ch);
+int main()  {
+    int n;
+    printf("enter number : ");
+    scanf("%d", &n);
+
+    int i=0;
+    while(i <= n) {
+        printf("%d\n",i);
+        i++;
+
     }
+
+    for(int i=1; i<=n; i++) {
+        printf("%d\n",i);
+    } 
     return 0;
 }    
+
