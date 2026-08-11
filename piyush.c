c25b4be74a2004333add0d2635f15b7165518852
jhap95631-prog <jhap95631@gmail.com>
2026-08-11 04:12:35 +0000

    print the table of number input by the user

--- piyush.c
+++ piyush.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
+
 int main() {
     int n;
     printf("enter number : ");
-    scanf("%d",&n);
-
-    int sum = 0;
-    for(int i=1; i<=n; i++) {
-        sum=sum + i;// sum += i
+    scanf("%d", &n);
+    
+    for(int i=1; i<=10; i++) {
+        printf("%d \n", n*i);
     }
         
-    printf("sum is %d \n",sum);
-
     return 0;
-}    
+    }
+        
+
+
+
+
+
+    
+    
 
 
 
