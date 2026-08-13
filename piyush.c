539fcc1f2412698182546692bb17c7da4f0b1f64
jhap95631-prog <jhap95631@gmail.com>
2026-08-13 08:07:21 +0000

    print a n number of tAbles by using passing argument

--- piyush.c
+++ piyush.c
@@ -1,45 +1,24 @@
 #include<stdio.h>
  
 int sum(int a, int b);
+void printtable(int n);
 
 int main() {
-      int a,b;
+      int n;
       printf("enter first number :");
-      scanf("%d", &a);
-      printf("enter second number ; ");
-      scanf("%d", &b);
-      
-      int s =sum(a,b);
-      printf("sum is : %d \n", s);
+      scanf("%d", &n);
+
+      printtable(n);//argument /actual parameter
+
       return 0;
-}      
-            
+}     
+
 int sum(int x, int y) {
-return x + y;
-}      
-
- 
-
-
-
-
-
-    
-    
-        
-
-
-
-
-    
-    
-
-
-
-   
-    
-
-
-
-    
-
+    return x + y;
+}
+      
+void printtable(int n) {//parameter/formal parmeter
+    for(int i=1; i<=10; i++) {
+        printf("%d\n", i*n);
+    }
+}         
